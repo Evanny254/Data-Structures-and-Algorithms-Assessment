301be4cc626cf151222294e3b8e54ee7454f5a20
Evanny254 <evancherry254@gmail.com>
2023-12-01 16:22:35 +0300

    Complete Question2

--- Question2.py
+++ Question2.py
@@ -14,9 +14,8 @@ def remove_duplicates(sequence):
 
     return result
 
-# Example input
+# Example 
 input_sequence = [2, 3, 2, 4, 5, 3, 6, 7, 5]
 
-# Test case
 result = remove_duplicates(input_sequence)
-print(result)  # Output: [2, 3, 4, 5, 6, 7]
+print(result)  
